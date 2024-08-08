02ea9130dc7c62aa43efbe2c76b9e4dee19f2094
Anatoly <20482479+trofimovdev@users.noreply.github.com>
2024-08-08 09:25:28 +0300

    wip

--- content.js
+++ content.js
@@ -24,19 +24,13 @@ window.addEventListener('message', function (e) {
         const data = JSON.parse(e.data.data);
         console.log(data, typeof data)
         console.log(data.state.articleId === lastPosting.id, data.state.articleId, lastPosting.id, data.state.articleBarcode === lastPosting.barcode, data.state.articleBarcode, lastPosting.barcode)
-        if (data.state.articleId === lastPosting.id && data.state.articleBarcode === lastPosting.barcode) {
+        if (data.state.articleId === lastPosting.id && data.state.articleBarcode === lastPosting.barcode && data.state.articleBarcode.startsWith('ii') && lastPosting.address !== "19") {
             // TODO: status Success
             console.log('print', lastPosting.address, lastPosting.barcode)
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://localhost:8000/print/" + lastPosting.address + "/" + lastPosting.barcode, true);
+            xhr.send(null);
         }
-
-        // console.log(lastPosting)
-
-        // let xhr = new XMLHttpRequest();
-        // xhr.open("POST", "http://localhost:8989", true);
-        // xhr.setRequestHeader('Content-Type', 'application/json');
-        // xhr.send(JSON.stringify({
-        //     hello: "world",
-        // }));
     }
 
 
@@ -45,7 +39,6 @@ window.addEventListener('message', function (e) {
     //     console.log('content script received:' , e.data.type, e.data.url, e.data.data);
     // }
 
-    // TODO: post request to backend
 });
 
 
