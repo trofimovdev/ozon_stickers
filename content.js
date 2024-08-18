8ef7721020eb0b0e8176729160409a173436af41
Anatoly <20482479+trofimovdev@users.noreply.github.com>
2024-08-18 12:57:10 +0300

    Update content.js

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ let lastPosting = {};
 // receive message from injected script
 window.addEventListener('message', function (e) {
     if (e.data.url === "/api/inbound/articles/resolve-v3") {
-        console.log('content script received:' , e.data.type, e.data.url, e.data.data);
+        console.log('/resolve-v3 received:' , e.data.type, e.data.url, e.data.data);
         const data = JSON.parse(e.data.data);
         lastPosting = {
             "id": data.id,
@@ -20,13 +20,14 @@ window.addEventListener('message', function (e) {
     }
 
     if (e.data.url === "/api/inbound/clearing/articles/receive") {
-        console.warn('content script received:' , e.data.type, e.data.url, e.data.data);
+        console.log('receive received:' , e.data.type, e.data.url, e.data.data);
         const data = JSON.parse(e.data.data);
         console.log(data, typeof data)
-        console.log(data.state.articleId === lastPosting.id, data.state.articleId, lastPosting.id, data.state.articleBarcode === lastPosting.barcode, data.state.articleBarcode, lastPosting.barcode)
+        console.log("preprint", data.state.articleId === lastPosting.id, data.state.articleId, lastPosting.id, data.state.articleBarcode === lastPosting.barcode, data.state.articleBarcode, lastPosting.barcode)
         if (data.state.articleId === lastPosting.id && data.state.articleBarcode === lastPosting.barcode && lastPosting.address !== "19" && data.state.status === "Success") {
             // TODO: status Success
-            console.log('print', lastPosting.address, lastPosting.barcode)
+            
+            console.warn('print', lastPosting.address, lastPosting.barcode)
             let xhr = new XMLHttpRequest();
             xhr.open("GET", "http://localhost:8000/print/" + lastPosting.address + "/" + lastPosting.barcode, true);
             xhr.send(null);
